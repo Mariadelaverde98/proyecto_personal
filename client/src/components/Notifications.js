dd9fb12128645d10cac2d1bfe3eb7219f9971fd1
mariadelaverde98 <mariadelaverde98@gmail.com>
2023-01-08 18:54:43 +0100

    stilos not

--- client/src/components/Notifications.js
+++ client/src/components/Notifications.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import "./styles/Notifications.css"
 
 const Notifications = (props) => {
     const [solicitudes, setSolicitudes] = useState([]);
@@ -23,10 +24,10 @@ const Notifications = (props) => {
     }
 
     function actualizarSolicitud(estado, id) {
-        
+
         let datos = {
             method: "post",
-            body: JSON.stringify({estado, id}),
+            body: JSON.stringify({ estado, id }),
             mode: "cors",
             headers: { "Access-Control-Allow-Origin": "*", "Content-type": "application/json" },
         };
@@ -37,22 +38,25 @@ const Notifications = (props) => {
 
     return (
         <div>
-            Solicitudes de etiquetas en fotos:
+            Solicitudes de etiquetas en fotos: <br />
             {solicitudes.length ? solicitudes.map((sol, i) => {
-                                return (
-                                    <div key={i}>
-                                        <img src={sol.publi.publication_path} alt="" />
-                                        <div>
-                                            <button onClick={() => actualizarSolicitud(1, sol.tag.id)}>
-                                                Accept
-                                            </button>
-                                            <button onClick={() => actualizarSolicitud(0, sol.tag.id)}>
-                                                Deny
-                                            </button>
-                                        </div>
-                                    </div>
-                                )
-                            }) : "No hay notificaciones"}
+                return (
+                    <div key={i} className="solicitudcontainer">
+                        <div className="containerimagensol">
+                            <img src={sol.publi.publication_path} alt="" />
+                        </div>
+
+                        <div className="buttonsContainer">
+                            <button className="aceptarsol" onClick={() => actualizarSolicitud(1, sol.tag.id)}>
+                                Accept
+                            </button>
+                            <button className="denegarsol" onClick={() => actualizarSolicitud(0, sol.tag.id)}>
+                                Deny
+                            </button>
+                        </div>
+                    </div>
+                )
+            }) : "No hay notificaciones"}
         </div>
     );
 };
